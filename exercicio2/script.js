ea21b5cdfd0902b06f612e40f3e04d324c11214f
igorazuos <azuosigor@gmail.com>
2022-08-31 14:25:23 -0300

    atualização exercicio2 a009

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,8 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaStringTrim = minhaString.trim()
+const minhaStringCompleta = minhaStringTrim.replace("________", "sticioso")
+console.log(minhaString)
+console.log(minhaString.length)
+console.log(minhaStringTrim.trim())
+console.log(minhaStringTrim.length)
+console.log(minhaStringCompleta)
