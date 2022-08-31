b0b9e8a2266ab9df0621140da065f91200c2361e
igorazuos <azuosigor@gmail.com>
2022-08-31 14:02:25 -0300

    atualização exercicio1 a009

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+const nomeUso = prompt("diga o seu nome")
+const comida1 = prompt("diga sua comida favorita")
+const comida2 = prompt("diga outra comida favorita")
+const comida3 = prompt ("diga outra comida favorita")
+
+console.log(`Estas são as comidas favoritas de ${nomeUso} 
+\n ${comida1}
+\n ${comida2}
+\n ${comida3} `)
